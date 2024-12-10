b833bba32d46c33afa2dfaaa97d6bb89c6a1ba9a
gregory <gredmyr@gmail.com>
2024-12-10 06:13:01 +0300

    Реализован базовый функционал

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -1,14 +1,14 @@
-import { NextFunction, Request, Response } from 'express';
-import { faker } from '@faker-js/faker';
-import Product from '../models/product';
-import { BadRequestError, ServerError } from '../errors';
+import { NextFunction, Request, Response } from "express";
+import { faker } from "@faker-js/faker";
+import Product from "../models/product";
+import { BadRequestError, ServerError } from "../errors";
 
 const createOrder = async (req: Request, res: Response, next: NextFunction) => {
   const { items, total } = req.body;
 
   try {
     if (!Array.isArray(items) || items.length === 0) {
-      return next(new BadRequestError('Массив элементов не может быть пустым'));
+      return next(new BadRequestError("Массив элементов не может быть пустым"));
     }
 
     const products = await Product.find({
@@ -18,11 +18,11 @@ const createOrder = async (req: Request, res: Response, next: NextFunction) => {
 
     const actualTotal = products.reduce(
       (acc, product) => acc + product.price,
-      0,
+      0
     );
 
     if (actualTotal !== total) {
-      return next(new BadRequestError('Неправильная сумма заказа'));
+      return next(new BadRequestError("Неправильная сумма заказа"));
     }
 
     const orderId = faker.number.hex({ min: 1000000000, max: 9999999999 });
